0c985d215dd4e81f118bb6d452a0992de5d8e35f
Jiahao Chen <horychen@qq.com>
2019-03-24 14:20:44 +0800

    t440 commit test.

--- codes/opti_script.py
+++ codes/opti_script.py
@@ -63,8 +63,8 @@ else: # NineSigma
     fea_config_dict['use_weights'] = 'O3'
     run_folder = r'run#194/' # optimize Qr=16 for O3
 
-    # fea_config_dict['mimic_separate_winding_with_DPNV_winding'] == True:
-    # run_folder = r'run#195/' # optimize Qr=16 for O3 - separate winding (with a reduced torque current down to 60%)
+    fea_config_dict['mimic_separate_winding_with_DPNV_winding'] = True
+    run_folder = r'run#195/' # optimize Qr=16 for O3 - separate winding (with a reduced torque current down to 60%)
 
 fea_config_dict['run_folder'] = run_folder
 logger = utility.myLogger(fea_config_dict['dir_codes'], prefix='ecce_'+run_folder[:-1])
