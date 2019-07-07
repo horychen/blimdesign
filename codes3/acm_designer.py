Fix a bug (forget a factor of 1.414) on Js and CurrentAmp. PMSM csv results collected. Cost as f1 is added.

codes3/acm_designer.py
@@ -62,8 +62,10 @@ class swarm_data_container(object):
         self.l_rated_iron_loss                      = [el[7] for el in self.rated_data]
         self.l_rated_windage_loss                   = [el[8] for el in self.rated_data]
         self.l_rated_rotor_volume                   = [el[9] for el in self.rated_data]
-        # self.l_rated_stack_length                   = [el[10] for el in self.rated_data] # new!
         self.l_rated_rotor_weight                   = [(V*8050*9.8) for V in self.l_rated_rotor_volume] # density of rotor is estimated to be that of steel of 8050 g/cm^3
+        self.l_rated_stack_length                   = [el[10] for el in self.rated_data] # new!
+        self.l_original_stack_length                = [el[11] for el in self.rated_data] # new!
+        self.l_original_rotor_weight                = [el/rated*ori for el, ori, rated in zip(self.l_rated_rotor_weight, self.l_rated_stack_length, self.l_original_stack_length)]
 
     #~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
     # Utility
@@ -127,7 +129,7 @@ class swarm_data_container(object):
 
         list_y_data = [ [el/1e3 for el in self.l_OA], 
                         self.l_OC,
-                        [F/W for W, F in zip(self.l_rated_rotor_weight, self.l_ss_avg_force_magnitude)], # FRW
+                        [F/W for W, F in zip(self.l_original_rotor_weight, self.l_ss_avg_force_magnitude)], # FRW
                         self.l_force_error_angle,
                         [100*el for el in self.l_OB], 
                         [100*el for el in self.l_normalized_force_error_magnitude],
@@ -661,7 +663,7 @@ class FEA_Solver:
         ################################################################
         # Load data for cost function evaluation
         ################################################################
-        results_to_be_unpacked = utility.build_str_results(self.axeses, spmsm_variant, self.project_name, study_name, self.dir_csv_output_folder, self.fea_config_dict, None)
+        results_to_be_unpacked = utility.build_str_results(self.axeses, spmsm_variant, self.project_name, study_name, self.dir_csv_output_folder, self.fea_config_dict, None, machine_type='PMSM')
         if results_to_be_unpacked is not None:
             self.fig_main.savefig(self.output_dir + spmsm_variant.name + 'results.png', dpi=150)
             utility.pyplot_clear(self.axeses)
@@ -840,7 +842,7 @@ class FEA_Solver:
         #~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
         # Load Results for Tran2TSS
         #~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
-        results_to_be_unpacked = utility.build_str_results(self.axeses, im_variant, self.project_name, tran2tss_study_name, self.dir_csv_output_folder, self.fea_config_dict, self.femm_solver)
+        results_to_be_unpacked = utility.build_str_results(self.axeses, im_variant, self.project_name, tran2tss_study_name, self.dir_csv_output_folder, self.fea_config_dict, self.femm_solver, machine_type='IM')
         if results_to_be_unpacked is not None:
             self.fig_main.savefig(self.output_dir + im_variant.name + 'results.png', dpi=150)
             utility.pyplot_clear(self.axeses)
@@ -973,7 +975,7 @@ class FEA_Solver:
         app.ExportImageWithSize(self.output_dir + model.GetName() + '.png', 2000, 2000)
         app.View().ShowModel() # 1st btn. close mesh view, and note that mesh data will be deleted if only ouput table results are selected.
 
-    def get_copper_loss_Bolognani(self, stator_slot_area, rotor_slot_area=None, STATOR_SLOT_FILL_FACTOR=0.5, ROTOR_SLOT_FILL_FACTOR=1.0, TEMPERATURE_OF_COIL=75, TORQUE_CURRENT_RATIO=0.975): 
+    def get_copper_loss_Bolognani(self, stator_slot_area, rotor_slot_area=None, STATOR_SLOT_FILL_FACTOR=0.5, ROTOR_SLOT_FILL_FACTOR=1.0, TEMPERATURE_OF_COIL=75, TORQUE_CURRENT_RATIO=0.975, design_parameters=None): 
         # make sure these two values 
         # space_factor_kCu = SLOT_FILL_FACTOR in Pyrhonen09 design
         # space_factor_kAl = 1 in Pyrhonen09 design
@@ -985,27 +987,22 @@ class FEA_Solver:
         #   Remember that the number_of_coil_per_slot (in series) is limited by the high back EMF rather than slot area.
         rho_Copper = (3.76*TEMPERATURE_OF_COIL+873)*1e-9/55. # resistivity
 
-        air_gap_length_delta     = self.im.design_parameters[0]*1e-3 # m
+        air_gap_length_delta     = design_parameters[0]*1e-3 # m (including sleeve, for PMSM)
 
         # http://127.0.0.1:4000/tech/ECCE-2019-Documentation/
 
         ################################################################
         # Stator Copper Loss 
         ################################################################
-        tooth_width_w_t          = self.im.design_parameters[1]*1e-3 # m
+        tooth_width_w_t          = design_parameters[1]*1e-3 # m
         Area_S_slot              = stator_slot_area
         area_copper_S_Cu         = STATOR_SLOT_FILL_FACTOR * Area_S_slot
-        a                        = self.im.wily.number_parallel_branch
-        zQ                       = self.im.DriveW_zQ
-        coil_pitch_yq            = self.im.wily.coil_pitch
-        Q                        = self.im.Qs
-        # the_radius_m             = 1e-3*(0.5*(self.im.Radius_OuterRotor + self.im.Length_AirGap + self.im.Radius_InnerStatorYoke))
-        stack_length_m           = 1e-3*self.im.stack_length
-        # number_of_phase          = 3
-        # Ns                       = zQ * self.im.Qs / (2 * number_of_phase * a) # 3 phase winding
-        # density_of_copper        = 8960 
-        # k_R                      = 1 # AC resistance factor
-        current_rms_value        = self.im.DriveW_CurrentAmp / 1.4142135623730951 * (1./TORQUE_CURRENT_RATIO) # for one phase
+        a                        = design_parameters[2]
+        zQ                       = design_parameters[3]
+        coil_pitch_yq            = design_parameters[4]
+        Q                        = design_parameters[5]
+        stack_length_m           = 1e-3*design_parameters[6]
+        current_rms_value        = design_parameters[7] / 1.4142135623730951 * (1./TORQUE_CURRENT_RATIO) # for one phase
         # Area_conductor_Sc        = Area_S_slot * STATOR_SLOT_FILL_FACTOR / zQ
 
         Js = (current_rms_value/a) * zQ / area_copper_S_Cu # 逆变器电流current_rms_value在流入电机时，
@@ -1013,8 +1010,8 @@ class FEA_Solver:
                                                            # 这样的电流在一个槽内有zQ个，所以Islot=(current_rms_value/a) * zQ
                                                            # 槽电流除以槽内铜的面积，就是电流密度
 
-        stator_inner_diameter_D = 2*(air_gap_length_delta + self.im.Radius_OuterRotor*1e-3)
-        slot_height_h_t = 0.5*(self.im.stator_yoke_diameter_Dsyi - stator_inner_diameter_D)
+        stator_inner_diameter_D = 2*(air_gap_length_delta + design_parameters[8]*1e-3)
+        slot_height_h_t = 0.5*(design_parameters[9] - stator_inner_diameter_D)
         slot_pitch_pps = np.pi * (stator_inner_diameter_D + slot_height_h_t) / Q
         kov = 1.8 # \in [1.6, 2.0]
         end_winding_length_Lew = np.pi*0.5 * (slot_pitch_pps + tooth_width_w_t) + slot_pitch_pps*kov * (coil_pitch_yq - 1)
@@ -1028,8 +1025,9 @@ class FEA_Solver:
         print('Stator current [Arms]:', current_rms_value, 'Js:', Js)
 
         if rotor_slot_area is not None:
+            raise Exception('Not supported')
             ################################################################
-            # Rotor Copper Loss
+            # Rotor Copper Loss (Valid for IM only)
             ################################################################
             tooth_width_w_t          = self.im.design_parameters[2]*1e-3 # m
             Area_S_slot              = rotor_slot_area
@@ -1064,7 +1062,7 @@ class FEA_Solver:
         else:
             rotor_copper_loss, rotor_copper_loss_along_stack, Jr = 0, 0, 0
 
-        return stator_copper_loss, rotor_copper_loss, stator_copper_loss_along_stack, rotor_copper_loss_along_stack, Js, Jr
+        return stator_copper_loss, rotor_copper_loss, stator_copper_loss_along_stack, rotor_copper_loss_along_stack, Js, Jr, Vol_Cu
 
 
 class acm_designer(object):

codes3/bearingless_spmsm_design.py
@@ -228,6 +228,8 @@ class bearingless_spmsm_design(bearingless_spmsm_template):
         self.ID = 'p%gs%g'%(self.p,self.s)
         self.number_current_generation = 0
         self.individual_index = counter
+        self.Radius_OuterStatorYoke = stator_outer_radius
+        self.stator_yoke_diameter_Dsyi = 2*(stator_outer_radius - self.mm_d_sy)
 
         # Parts
         self.rotorCore = CrossSectInnerNotchedRotor.CrossSectInnerNotchedRotor(
@@ -275,6 +277,7 @@ class bearingless_spmsm_design(bearingless_spmsm_template):
         self.DriveW_Freq       = spmsm_template.DriveW_Freq      
         self.DriveW_Rs         = spmsm_template.DriveW_Rs        
         self.DriveW_zQ         = spmsm_template.DriveW_zQ
+        print('---DriveW_zQ:', self.DriveW_zQ)
         print('---Template CurrentAmp:', spmsm_template.DriveW_CurrentAmp)
         self.DriveW_CurrentAmp = None ########### will be assisned when drawing the coils
         self.DriveW_poles      = spmsm_template.DriveW_poles
@@ -375,12 +378,14 @@ class bearingless_spmsm_design(bearingless_spmsm_template):
         toolJd.iRotateCopy = self.coils.stator_core.Q
         region4 = toolJd.prepareSection(list_regions)
 
-        current_in_the_slot = self.coils.slot_area * self.Js*1e-6 * self.fill_factor
-        variant_DriveW_CurrentAmp = current_in_the_slot / self.DriveW_zQ * self.wily.number_parallel_branch # DriveW_CurrentAmp is the phase current seen from the inverter. 
+        CurrentAmp_in_the_slot = self.coils.mm2_slot_area * self.fill_factor * self.Js*1e-6 * np.sqrt(2) #/2.2*2.8
+        CurrentAmp_per_conductor = CurrentAmp_in_the_slot / self.DriveW_zQ
+        CurrentAmp_per_phase = CurrentAmp_per_conductor * self.wily.number_parallel_branch # 跟几层绕组根本没关系！除以zQ的时候，就已经变成每根导体的电流了。
+        variant_DriveW_CurrentAmp = CurrentAmp_per_phase
         self.DriveW_CurrentAmp = variant_DriveW_CurrentAmp ########### will be assisned when drawing the coils
         self.BeariW_CurrentAmp = 0.025 * self.DriveW_CurrentAmp/0.975 # extra 2.5% as bearing current
-        print('---Variant current_in_the_slot =', current_in_the_slot)
-        print('---Variant CurrentAmp = current_in_the_slot / self.DriveW_zQ * self.wily.number_parallel_branch =', variant_DriveW_CurrentAmp)
+        print('---Variant CurrentAmp_in_the_slot =', CurrentAmp_in_the_slot)
+        print('---variant_DriveW_CurrentAmp = CurrentAmp_per_phase =', variant_DriveW_CurrentAmp)
 
         # Import Model into Designer
         toolJd.doc.SaveModel(False) # True: Project is also saved. 
@@ -551,8 +556,9 @@ class bearingless_spmsm_design(bearingless_spmsm_template):
         study.GetCondition("RotCon").SetValue("AngularVelocity", int(self.the_speed))
         study.GetCondition("RotCon").ClearParts()
         study.GetCondition("RotCon").AddSet(model.GetSetList().GetSet("Motion_Region"), 0)
-        # d-axis initial position is self.deg_alpha_rm*0.5
-        # The U-phase current is sin(omega_syn*t) = 0 at t=0.
+        # Implementation of id=0 control:
+        #   d-axis initial position is self.deg_alpha_rm*0.5
+        #   The U-phase current is sin(omega_syn*t) = 0 at t=0.
         study.GetCondition("RotCon").SetValue(u"InitialRotationAngle", -self.deg_alpha_rm*0.5 + 90 + self.wily.initial_excitation_bias_compensation_deg + (180/self.p)) # add 360/(2p) deg to reverse the initial magnetizing direction to make torque positive.
 
 
@@ -832,6 +838,8 @@ class bearingless_spmsm_design(bearingless_spmsm_template):
             study.GetCircuit().GetSubCircuit("Star Connection %d"%(poles)).GetComponent("Coil1").SetName("CircuitCoil%dU"%(poles))
             study.GetCircuit().GetSubCircuit("Star Connection %d"%(poles)).GetComponent("Coil2").SetName("CircuitCoil%dV"%(poles))
             study.GetCircuit().GetSubCircuit("Star Connection %d"%(poles)).GetComponent("Coil3").SetName("CircuitCoil%dW"%(poles))
+            # Star Connection_2 is GroupAC
+            # Star Connection_4 is GroupBD
 
             if bool_3PhaseCurrentSource == True: # must use this for frequency analysis
 