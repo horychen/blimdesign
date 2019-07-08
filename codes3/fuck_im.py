a423699c89b64a7673fb544acff78a8673217ff6
Jiahao Chen <Jiahao.Chen@wisc.edu>
2019-07-08 18:01:09 -0500

    Auto restart only interrupted by KeyboardInterrupt.

--- codes3/fuck_im.py
+++ codes3/fuck_im.py
@@ -1,8 +1,11 @@
 from utility import my_execfile, send_notification
 counter_auto_restart = 0
-try:
-    my_execfile('./one_script.py', g=globals(), l=locals())
-except Exception as error:
-    counter_auto_restart += 1
-    send_notification(text = ad.solver.fea_config_dict['pc_name'] + str(error) + '\n'*3,
-                      subject = '[%s] Auto Restart Run #%d'%(ad.solver.fea_config_dict['pc_name'], counter_auto_restart))
+while True:
+    try:
+        my_execfile('./one_script.py', g=globals(), l=locals())
+    except KeyboardInterrupt as error:
+        break
+    except Exception as error:
+        counter_auto_restart += 1
+        send_notification(text = ad.solver.fea_config_dict['pc_name'] + str(error) + '\n'*3,
+                          subject = '[%s] Auto Restart Run #%d'%(ad.solver.fea_config_dict['pc_name'], counter_auto_restart))
