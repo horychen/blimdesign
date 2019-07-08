e99a6c200810008f92f7bd16ff8a9caa3aa297c3
Jiahao Chen <Jiahao.Chen@wisc.edu>
2019-07-08 01:55:38 -0500

    d_pm is not defined

--- codes3/CrossSectInnerNotchedRotor.py
+++ codes3/CrossSectInnerNotchedRotor.py
@@ -68,8 +68,8 @@ class CrossSectInnerNotchedRotor(object):
         s        = self.s
         alpha_rp = 2*np.pi/(2*p) # pole span
 
-        if abs(d_pm - d_rp) < 2*EPS:
-            print('Warn: [Notched Rotor] Detect d_rp is too close to d_pm. To avoid small line entity error in JMAG, will set d_pm equal to d_rp in CrossSectInnerNotchedMagnet.') # d_pm is not defined here so we cannot set d_rp to d_pm.
+        # if abs(d_pm - d_rp) < 2*EPS: d_pm is not defined
+        #     print('Warn: [Notched Rotor] Detect d_rp is too close to d_pm. To avoid small line entity error in JMAG, will set d_pm equal to d_rp in CrossSectInnerNotchedMagnet.') # d_pm is not defined here so we cannot set d_rp to d_pm.
 
         P1 = [r_ri, 0]
 
