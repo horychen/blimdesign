Add exclusive post-processing codes. Find bugs in copper loss calculation: 1. the Vol_Cu_along_stack = area_copper_S_Cu * (end_winding_length_Lew) * Q is wrong; 2. coil_pitch_yq            = abs(copper_loss_parameters[4]) # coil pitch is -1 for concentrated winding so an abs() is required.

codes3/bearingless_spmsm_design.py
@@ -159,19 +159,19 @@ class bearingless_spmsm_design(bearingless_spmsm_template):
                     else:
                         raise Exception('Not tested feature. Add your referece for this free variable.')
 
-        deg_alpha_st             = free_variables[0]  # if self.filter[0]  else spmsm_template.deg_alpha_st            
-        mm_d_so                  = free_variables[1]  # if self.filter[1]  else spmsm_template.mm_d_so                 
-        mm_d_st                  = free_variables[2]  # if self.filter[2]  else spmsm_template.mm_d_st                 
-        stator_outer_radius      = free_variables[3]  # if self.filter[3]  else spmsm_template.stator_outer_radius     
-        mm_w_st                  = free_variables[4]  # if self.filter[4]  else spmsm_template.mm_w_st                 
-        sleeve_length            = free_variables[5]  # if self.filter[5]  else spmsm_template.sleeve_length           
-        mm_d_pm                  = free_variables[6]  # if self.filter[6]  else spmsm_template.mm_d_pm                 
-        deg_alpha_rm             = free_variables[7]  # if self.filter[7]  else spmsm_template.deg_alpha_rm            
-        deg_alpha_rs             = free_variables[8]  # if self.filter[8]  else deg_alpha_rm
-        mm_d_ri                  = free_variables[9]  # if self.filter[9]  else spmsm_template.mm_d_ri                 
-        rotor_steel_outer_radius = free_variables[10] # if self.filter[10] else spmsm_template.rotor_steel_outer_radius
-        mm_d_rp                  = free_variables[11] # if self.filter[11] else spmsm_template.mm_d_rp                 
-        mm_d_rs                  = free_variables[12] # if self.filter[12] else spmsm_template.mm_d_rs                 
+        deg_alpha_st                = free_variables[0]  # if self.filter[0]  else spmsm_template.deg_alpha_st            
+        mm_d_so                     = free_variables[1]  # if self.filter[1]  else spmsm_template.mm_d_so                 
+        mm_d_st                     = free_variables[2]  # if self.filter[2]  else spmsm_template.mm_d_st                 
+        stator_outer_radius         = free_variables[3]  # if self.filter[3]  else spmsm_template.stator_outer_radius     
+        mm_w_st                     = free_variables[4]  # if self.filter[4]  else spmsm_template.mm_w_st                 
+        sleeve_length               = free_variables[5]  # if self.filter[5]  else spmsm_template.sleeve_length           
+        mm_d_pm                     = free_variables[6]  # if self.filter[6]  else spmsm_template.mm_d_pm                 
+        deg_alpha_rm                = free_variables[7]  # if self.filter[7]  else spmsm_template.deg_alpha_rm            
+        deg_alpha_rs                = free_variables[8]  # if self.filter[8]  else deg_alpha_rm
+        mm_d_ri                     = free_variables[9]  # if self.filter[9]  else spmsm_template.mm_d_ri                 
+        mm_rotor_steel_outer_radius = free_variables[10] # if self.filter[10] else spmsm_template.mm_rotor_steel_outer_radius
+        mm_d_rp                     = free_variables[11] # if self.filter[11] else spmsm_template.mm_d_rp                 
+        mm_d_rs                     = free_variables[12] # if self.filter[12] else spmsm_template.mm_d_rs                 
 
         if mm_d_rp > mm_d_pm:
             mm_d_rp = mm_d_pm
@@ -182,7 +182,7 @@ class bearingless_spmsm_design(bearingless_spmsm_template):
 
         self.deg_alpha_st = free_variables[0]
         self.deg_alpha_so = spmsm_template.deg_alpha_so
-        self.mm_r_si      = rotor_steel_outer_radius + (mm_d_pm - mm_d_rp) + sleeve_length + spmsm_template.fixed_air_gap_length
+        self.mm_r_si      = mm_rotor_steel_outer_radius + (mm_d_pm - mm_d_rp) + sleeve_length + spmsm_template.fixed_air_gap_length
         self.mm_d_so      = free_variables[1]
         self.mm_d_sp      = 1.5*self.mm_d_so # The neck is 0.5 of the head as IM's geometry.
         self.mm_d_st      = free_variables[2]; stator_outer_radius = free_variables[3]
@@ -197,8 +197,8 @@ class bearingless_spmsm_design(bearingless_spmsm_template):
         self.mm_d_pm      = free_variables[6]
         self.deg_alpha_rm = free_variables[7]
         self.deg_alpha_rs = free_variables[8]
-        self.mm_d_ri      = free_variables[9]; rotor_steel_outer_radius = free_variables[10]
-        self.mm_r_ri      = rotor_steel_outer_radius - mm_d_rp - mm_d_ri
+        self.mm_d_ri      = free_variables[9]; mm_rotor_steel_outer_radius = free_variables[10]
+        self.mm_r_ri      = mm_rotor_steel_outer_radius - mm_d_rp - mm_d_ri
         self.mm_d_rp      = free_variables[11]
         self.mm_d_rs      = free_variables[12]
         self.p = spmsm_template.p
@@ -235,8 +235,8 @@ class bearingless_spmsm_design(bearingless_spmsm_template):
         #                                 ]
 
         # obsolete variable from IM
-        self.Radius_OuterRotor = rotor_steel_outer_radius  + (mm_d_pm - mm_d_rp) # the outer radius of the rotor with magnet / the magnet
-        self.Length_AirGap = self.fixed_air_gap_length
+        self.Radius_OuterRotor = mm_rotor_steel_outer_radius  + (mm_d_pm - mm_d_rp) # the outer radius of the rotor with magnet / the magnet
+        self.Length_AirGap = self.fixed_air_gap_length + self.sleeve_length
         self.ID = 'p%gs%g'%(self.p,self.s)
         self.number_current_generation = 0
         self.individual_index = counter
@@ -360,7 +360,7 @@ class bearingless_spmsm_design(bearingless_spmsm_template):
         else:
             raise Exception('Not implemented.')
 
-    def draw_spmsm(self, toolJd):
+    def draw_spmsm(self, toolJd, bool_pyx=False):
 
         # Rotor Core
         list_segments = self.rotorCore.draw(toolJd)
@@ -369,10 +369,11 @@ class bearingless_spmsm_design(bearingless_spmsm_template):
         region1 = toolJd.prepareSection(list_segments)
 
         # Shaft
-        list_segments = self.shaft.draw(toolJd)
-        toolJd.bMirror = False
-        toolJd.iRotateCopy = 1
-        region0 = toolJd.prepareSection(list_segments)
+        if not bool_pyx:
+            list_segments = self.shaft.draw(toolJd)
+            toolJd.bMirror = False
+            toolJd.iRotateCopy = 1
+            region0 = toolJd.prepareSection(list_segments)
 
         # Rotor Magnet
         list_regions = self.rotorMagnet.draw(toolJd)
@@ -381,15 +382,16 @@ class bearingless_spmsm_design(bearingless_spmsm_template):
         region2 = toolJd.prepareSection(list_regions, bRotateMerge=False)
 
         # Sleeve
-        sleeve = CrossSectInnerNotchedRotor.CrossSectSleeve(
-                        name = 'Sleeve',
-                        notched_magnet = self.rotorMagnet,
-                        d_sleeve = self.sleeve_length
-                        )
-        list_regions = sleeve.draw(toolJd)
-        toolJd.bMirror = False
-        toolJd.iRotateCopy = self.rotorMagnet.notched_rotor.p*2
-        regionS = toolJd.prepareSection(list_regions)
+        if not bool_pyx:
+            sleeve = CrossSectInnerNotchedRotor.CrossSectSleeve(
+                            name = 'Sleeve',
+                            notched_magnet = self.rotorMagnet,
+                            d_sleeve = self.sleeve_length
+                            )
+            list_regions = sleeve.draw(toolJd)
+            toolJd.bMirror = False
+            toolJd.iRotateCopy = self.rotorMagnet.notched_rotor.p*2
+            regionS = toolJd.prepareSection(list_regions)
 
         # Stator Core
         list_regions = self.stator_core.draw(toolJd)
@@ -397,35 +399,36 @@ class bearingless_spmsm_design(bearingless_spmsm_template):
         toolJd.iRotateCopy = self.stator_core.Q
         region3 = toolJd.prepareSection(list_regions)
 
-        # Stator Winding
-        list_regions = self.coils.draw(toolJd)
-        toolJd.bMirror = False
-        toolJd.iRotateCopy = self.coils.stator_core.Q
-        region4 = toolJd.prepareSection(list_regions)
+        if not bool_pyx:
+            # Stator Winding
+            list_regions = self.coils.draw(toolJd)
+            toolJd.bMirror = False
+            toolJd.iRotateCopy = self.coils.stator_core.Q
+            region4 = toolJd.prepareSection(list_regions)
 
-        CurrentAmp_in_the_slot = self.coils.mm2_slot_area * self.fill_factor * self.Js*1e-6 * np.sqrt(2) #/2.2*2.8
-        CurrentAmp_per_conductor = CurrentAmp_in_the_slot / self.DriveW_zQ
-        CurrentAmp_per_phase = CurrentAmp_per_conductor * self.wily.number_parallel_branch # 跟几层绕组根本没关系！除以zQ的时候，就已经变成每根导体的电流了。
-        variant_DriveW_CurrentAmp = CurrentAmp_per_phase # this current amp value is for non-bearingless motor
-        self.CurrentAmp_per_phase = CurrentAmp_per_phase
-        self.DriveW_CurrentAmp = self.fea_config_dict['TORQUE_CURRENT_RATIO'] * variant_DriveW_CurrentAmp 
-        self.BeariW_CurrentAmp = self.fea_config_dict['SUSPENSION_CURRENT_RATIO'] * variant_DriveW_CurrentAmp
+            CurrentAmp_in_the_slot = self.coils.mm2_slot_area * self.fill_factor * self.Js*1e-6 * np.sqrt(2) #/2.2*2.8
+            CurrentAmp_per_conductor = CurrentAmp_in_the_slot / self.DriveW_zQ
+            CurrentAmp_per_phase = CurrentAmp_per_conductor * self.wily.number_parallel_branch # 跟几层绕组根本没关系！除以zQ的时候，就已经变成每根导体的电流了。
+            variant_DriveW_CurrentAmp = CurrentAmp_per_phase # this current amp value is for non-bearingless motor
+            self.CurrentAmp_per_phase = CurrentAmp_per_phase
+            self.DriveW_CurrentAmp = self.fea_config_dict['TORQUE_CURRENT_RATIO'] * variant_DriveW_CurrentAmp 
+            self.BeariW_CurrentAmp = self.fea_config_dict['SUSPENSION_CURRENT_RATIO'] * variant_DriveW_CurrentAmp
 
-        slot_area_utilizing_ratio = (self.DriveW_CurrentAmp + self.BeariW_CurrentAmp) / self.CurrentAmp_per_phase
-        print('---Heads up! slot_area_utilizing_ratio is', slot_area_utilizing_ratio)
+            slot_area_utilizing_ratio = (self.DriveW_CurrentAmp + self.BeariW_CurrentAmp) / self.CurrentAmp_per_phase
+            print('---Heads up! slot_area_utilizing_ratio is', slot_area_utilizing_ratio)
 
-        print('---Variant CurrentAmp_in_the_slot =', CurrentAmp_in_the_slot)
-        print('---variant_DriveW_CurrentAmp = CurrentAmp_per_phase =', variant_DriveW_CurrentAmp)
-        print('---self.DriveW_CurrentAmp =', self.DriveW_CurrentAmp)
-        print('---self.BeariW_CurrentAmp =', self.BeariW_CurrentAmp)
-        print('---TORQUE_CURRENT_RATIO:', self.fea_config_dict['TORQUE_CURRENT_RATIO'])
-        print('---SUSPENSION_CURRENT_RATIO:', self.fea_config_dict['SUSPENSION_CURRENT_RATIO'])
+            print('---Variant CurrentAmp_in_the_slot =', CurrentAmp_in_the_slot)
+            print('---variant_DriveW_CurrentAmp = CurrentAmp_per_phase =', variant_DriveW_CurrentAmp)
+            print('---self.DriveW_CurrentAmp =', self.DriveW_CurrentAmp)
+            print('---self.BeariW_CurrentAmp =', self.BeariW_CurrentAmp)
+            print('---TORQUE_CURRENT_RATIO:', self.fea_config_dict['TORQUE_CURRENT_RATIO'])
+            print('---SUSPENSION_CURRENT_RATIO:', self.fea_config_dict['SUSPENSION_CURRENT_RATIO'])
 
-        # Import Model into Designer
-        toolJd.doc.SaveModel(False) # True: Project is also saved. 
-        model = toolJd.app.GetCurrentModel()
-        model.SetName(self.name)
-        model.SetDescription(self.show(toString=True))
+            # Import Model into Designer
+            toolJd.doc.SaveModel(False) # True: Project is also saved. 
+            model = toolJd.app.GetCurrentModel()
+            model.SetName(self.name)
+            model.SetDescription(self.show(toString=True))
 
         return True
 

codes3/one_script_pm.py
@@ -4,7 +4,7 @@ import utility
 from utility import my_execfile
 import utility_moo
 from win32com.client import pywintypes
-bool_post_processing = False # solve or post-processing
+bool_post_processing = True # solve or post-processing
 bool_re_evaluate = False
 
 #~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
@@ -13,25 +13,21 @@ bool_re_evaluate = False
 # FEA setting
 my_execfile('./default_setting.py', g=globals(), l=locals())
 fea_config_dict
-fea_config_dict['Active_Qr'] = 16 # obsolete 
-fea_config_dict['local_sensitivity_analysis'] = False
-fea_config_dict['bool_refined_bounds'] = False
-fea_config_dict['use_weights'] = 'O2' # this is not used
 if True:
+    # PEMD2020
+
     # Combined winding PMSM
     fea_config_dict['TORQUE_CURRENT_RATIO'] = 0.95
     fea_config_dict['SUSPENSION_CURRENT_RATIO'] = 0.05
-    run_folder = r'run#610/' # FRW constraint is added and sleeve_length is 3 (not varying). Excitation ratio is 95%:5% between Torque and Suspension windings.
-    run_folder = r'run#611/' # zero Rs is not allowed
-    run_folder = r'run#61495/' # spec_PEMD_BPMSM_Q12p2, 99 zQ is fixed to 10 | 98 zQ is derived | 97 sleeve length is reduced to 1 mm | 96 Jingwei's layout | 95 alpha_rm is fixed to be 360/2/p | 94 full alpha_rm bug is fixed | )
+        # run_folder = r'run#610/' # FRW constraint is added and sleeve_length is 3 (not varying). Excitation ratio is 95%:5% between Torque and Suspension windings.
+        # run_folder = r'run#611/' # zero Rs is not allowed
+        # run_folder = r'run#61495/' # spec_PEMD_BPMSM_Q12p2, 99 zQ is fixed to 10 | 98 zQ is derived | 97 sleeve length is reduced to 1 mm | 96 Jingwei's layout | 95 alpha_rm is fixed to be 360/2/p | 94 full alpha_rm bug is fixed | )
 
     # run_folder = r'run#62399/' # spec_ECCE_PMSM_ (Q6p2)
     # run_folder = r'run#62499/' # spec_PEMD_BPMSM_Q12p2
     # run_folder = r'run#62599/' # spec_PEMD_BPMSM_Q6p1)
     # run_folder = r'run#62699/' # spec_PEMD_BPMSM_Q12p4)
     run_folder = r'run#62799/' # spec_PEMD_BPMSM_Q24p1
-
-    fea_config_dict['run_folder'] = run_folder
 
     # spec's
     # my_execfile('./spec_ECCE_PMSM_.py', g=globals(), l=locals()) # Q=6, p=2
@@ -40,10 +36,13 @@ if True:
     # my_execfile('./spec_PEMD_BPMSM_Q12p4.py', g=globals(), l=locals()) # Q=12, p=4, ps=5
     my_execfile('./spec_PEMD_BPMSM_Q24p1.py', g=globals(), l=locals()) # Q=24, p=1, ps=2
 
+    fea_config_dict['run_folder'] = run_folder
 
     # Adopt Bianchi 2006 for a SPM motor template
     spec.build_pmsm_template(fea_config_dict, im_template=None)
 else:
+    # TIA_ITEC2018_PAPER
+
     if 'Y730' in fea_config_dict['pc_name']:
         ################################################################
         # Y730
@@ -98,9 +97,9 @@ else:
     spec.build_im_template(fea_config_dict)
     spec.build_pmsm_template(fea_config_dict, im_template=spec.im_template)
 
-# select motor type ehere
-print('Build ACM template...')
+# select motor type here
 spec.acm_template = spec.pmsm_template
+print('Build ACM template...')
 
 import acm_designer
 global ad
@@ -132,254 +131,11 @@ if ad.bool_re_evaluate:
 import pygmo as pg
 global counter_fitness_called, counter_fitness_return
 from acm_designer import get_bad_fintess_values
-
-class Problem_BearinglessSynchronousDesign(object):
-
-    # Define objectives
-    def fitness(self, x):
-        global ad, counter_fitness_called, counter_fitness_return
-
-        if ad.flag_do_not_evaluate_when_init_pop == True:
-            return [0, 0, 0]
-
-        ad, counter_fitness_called, counter_fitness_return
-        if counter_fitness_called == counter_fitness_return:
-            counter_fitness_called += 1
-        else:
-            # This is not reachable
-            raise Exception('counter_fitness_called')
-        print('Call fitness: %d, %d'%(counter_fitness_called, counter_fitness_return))
-
-        # 不要标幺化了！统一用真的bounds，见get_bounds()
-        x_denorm = x
-
-        # evaluate x_denorm via FEA tools
-        counter_loop = 0
-        stuck_at = 0
-        while True:
-            if ad.bool_re_evaluate:
-                if counter_fitness_return >= len(ad.solver.swarm_data):
-                    quit()
-                x_denorm = ad.solver.swarm_data[counter_fitness_return][:-3]
-                print(ad.solver.swarm_data[counter_fitness_return])
-
-            if stuck_at < counter_fitness_called:
-                stuck_at = counter_fitness_called
-                counter_loop = 0 # reset
-            if stuck_at == counter_fitness_called:
-                counter_loop += 1
-
-            # if True:
-            try:
-                cost_function, f1, f2, f3, FRW, \
-                normalized_torque_ripple, \
-                normalized_force_error_magnitude, \
-                force_error_angle = \
-                    ad.evaluate_design(ad.spec.acm_template, x_denorm, counter_fitness_called, counter_loop=counter_loop)
-
-                # remove folder .jfiles to save space (we have to generate it first in JMAG Designer to have field data and voltage profiles)
-                if ad.solver.folder_to_be_deleted is not None and os.path.isdir(ad.solver.folder_to_be_deleted):
-                    try:
-                        shutil.rmtree(ad.solver.folder_to_be_deleted) # .jfiles directory
-                    except PermissionError as error:
-                        print(error)
-                        print('Skip deleting this folder...')
-                # update to be deleted when JMAG releases the use
-                ad.solver.folder_to_be_deleted = ad.solver.expected_project_file[:-5]+'jfiles'
-            except KeyboardInterrupt as error:
-                raise error
-            except Exception as error:
-                # raise error
-                f1, f2, f3 = get_bad_fintess_values(machine_type='PMSM')
-                print(str(error))
-                logger = logging.getLogger(__name__)
-                logger.error(str(error))
-                break
-                # except FileNotFoundError as error: # The copy region target is not found
-                #     print(str(error))
-                #     print('CJH: "the ind***TranPMSM_torque.csv is not found" means the mesher or the solver has failed. For now, simply consider it to be bad design.')
-                #     f1, f2, f3 = get_bad_fintess_values(machine_type='PMSM')
-
-                # except utility.ExceptionBadNumberOfParts as error:
-                #     print(str(error)) 
-                #     # print("Detail: {}".format(error.payload))
-                #     f1, f2, f3 = get_bad_fintess_values(machine_type='PMSM')
-                #     utility.send_notification(ad.solver.fea_config_dict['pc_name'] + '\n\nExceptionBadNumberOfParts:' + str(error) + '\n'*3)
-                #     break
-
-                # except (utility.ExceptionReTry, pywintypes.com_error) as error:
-                #     print(error)
-
-                #     msg = 'FEA tool failed for individual #%d: attemp #%d.'%(counter_fitness_called, counter_loop)
-                #     logger = logging.getLogger(__name__)
-                #     logger.error(msg)
-                #     print(msg)
-
-                #     if counter_loop > 1: # > 1 = two attemps; > 2 = three attemps
-                #         print(error)
-                #         raise Exception('Abort the optimization. Two attemps to evaluate the design have all failed for individual #%d'%(counter_fitness_called))
-                #     else:
-                #         from time import sleep
-                #         print('\n\n\nSleep for 3 sec and continue.')
-                #         sleep(3)
-                #         continue
-
-                # except AttributeError as error:
-                #     print(str(error)) 
-                #     # print("Detail: {}".format(error.payload))
-
-                #     msg = 'FEA tool failed for individual #%d: attemp #%d.'%(counter_fitness_called, counter_loop)
-                #     logger = logging.getLogger(__name__)
-                #     logger.error(msg)
-                #     print(msg)
-
-                #     if 'designer.Application' in str(error):
-                #         if counter_loop > 1: 
-                #             print(error)
-                #             raise Exception('Abort the optimization. Two attemps to evaluate the design have all failed for individual #%d'%(counter_fitness_called))
-                #         else:
-                #             from time import sleep
-                #             print('\n\n\nSleep for 3 sec and continue.')
-                #             sleep(3)                        
-                #             continue
-                #     else:
-                #         raise error
-
-                # except Exception as e: # raise and need human inspection
-
-                #     # raise e
-                #     print('-'*40 + 'Unexpected error is caught.')
-                #     print(str(e)) 
-                #     utility.send_notification(ad.solver.fea_config_dict['pc_name'] + '\n\nUnexpected expection:' + str(e))
-                #     raise e
-            else:
-                # - Price
-                f1 
-                # - Efficiency @ Rated Power
-                f2 
-                # Ripple Performance (Weighted Sum)
-                f3 
-                print('f1,f2,f3:',f1,f2,f3)
-
-                try:
-                    # Constraints (Em<0.2 and Ea<10 deg):
-                    # if abs(normalized_torque_ripple)>=0.2 or abs(normalized_force_error_magnitude) >= 0.2 or abs(force_error_angle) > 10 or SafetyFactor < 1.5:
-                    # if abs(normalized_torque_ripple)>=0.2 or abs(normalized_force_error_magnitude) >= 0.2 or abs(force_error_angle) > 10 or FRW < 1:
-                    # if abs(normalized_torque_ripple)>=0.2 or abs(normalized_force_error_magnitude) >= 0.2 or abs(force_error_angle) > 10:
-                    if abs(normalized_torque_ripple)>=0.3 or abs(normalized_force_error_magnitude) >= 0.35 or abs(force_error_angle) > 20 or FRW < 0.75:
-                        print('Constraints are violated:')
-                        if abs(normalized_torque_ripple)>=0.3:
-                            print('\tabs(normalized_torque_ripple)>=0.3 | (=%f)' % (normalized_torque_ripple))
-                        if abs(normalized_force_error_magnitude) >= 0.35:
-                            print('\tabs(normalized_force_error_magnitude) >= 0.35 | (=%f)' % (normalized_force_error_magnitude))
-                        if abs(force_error_angle) > 20:
-                            print('\tabs(force_error_angle) > 20 | (=%f)' % (force_error_angle))
-                        if FRW < 0.75:
-                            print('\tFRW < 0.75 | (=%f)' % (FRW))
-                        f1, f2, f3 = get_bad_fintess_values(machine_type='PMSM')
-                    print('f1,f2,f3:',f1,f2,f3)
-                except:
-                    msg = 'This design causes an error in JMAG and hence is discarded..'
-                    print(msg)
-                    logger = logging.getLogger(__name__)
-                    logger.warn(msg)
-
-                break
-
-        counter_fitness_return += 1
-        print('Fitness: %d, %d\n----------------'%(counter_fitness_called, counter_fitness_return))
-        # raise KeyboardInterrupt
-        return [f1, f2, f3]
-
-    # Return number of objectives
-    def get_nobj(self):
-        return 3
-
-    # Return bounds of decision variables (a.k.a. chromosome)
-    def get_bounds(self):
-        global ad
-        print('Problem_BearinglessSynchronousDesign.get_bounds:', ad.bounds_denorm)
-        min_b, max_b = np.asarray(ad.bounds_denorm).T 
-        return ( min_b.tolist(), max_b.tolist() )
-
-    # Return function name
-    def get_name(self):
-        return "Bearingless PMSM Design"
-
+from acm_designer import Problem_BearinglessSynchronousDesign
 
 if bool_post_processing == True:
-    # Combine all data 
-
-    # Select optimal design by user-defined criteria
-    if r'run#62' in fea_config_dict['run_folder']:
-        ad.solver.output_dir = ad.solver.fea_config_dict['dir_parent'] + ad.solver.fea_config_dict['run_folder'] 
-        number_of_chromosome = ad.solver.read_swarm_data(ad.bound_filter)
-        swarm_data_Y730 = ad.solver.swarm_data
-        swarm_project_names_Y730 = ad.solver.swarm_data_container.project_names
-        print('number_of_chromosome =', number_of_chromosome)
- 
-        def selection_criteria(swarm_data_):
-            global best_idx, best_chromosome
-            # HIGH TORQUE DENSITY
-            # Y730
-            #       L_g,    w_st,   w_rt,   theta_so,   w_ro,    d_so,    d_ro,    -TRV,    -eta,    OC.
-            # 794 [1.16163, 9.00566, 8.34039, 2.91474, 0.786231, 2.76114, 1.2485, -22666.7, -0.953681, 4.89779]
-            # ----------------------------------------
-            # HIGH EFFICIENCY
-            # Y730
-            #       L_g,    w_st,   w_rt,   theta_so,   w_ro,    d_so,    d_ro,    -TRV,    -eta,    OC.
-            # 186 [1.25979, 6.80075, 5.64435, 5.8548, 1.59461, 2.11656, 2.58401, -17633.3, -0.958828, 5.53104]
-            # 615 [1.2725, 5.6206, 4.60947, 3.56502, 2.27635, 0.506179, 2.78758, -17888.9, -0.958846, 8.56211]
-            # ----------------------------------------
-            # LOW RIPPLE PERFORMANCE
-            # Severson02
-            #       L_g,    w_st,   w_rt,   theta_so,   w_ro,    d_so,    d_ro,    -TRV,    -eta,    OC.
-            # 1043 [1.38278, 8.91078, 7.43896, 2.66259, 0.611812, 1.50521, 1.51402, -19125.4, -0.953987, 2.91096]
-            # 1129 [1.38878, 8.68378, 7.97301, 2.82904, 0.586374, 1.97867, 1.45825, -19169.0, -0.954226, 2.99944]
-            # 1178 [1.36258, 8.9625, 7.49621, 2.5878, 0.503512, 0.678909, 1.74283, -19134.3, -0.952751, 2.90795]
-
-            for idx, chromosome in enumerate(swarm_data_):
-                # if chromosome[-1] < 5 and chromosome[-2] < -0.95 and chromosome[-3] < -22500: # best Y730     #1625, 0.000702091 * 8050 * 9.8 = 55.38795899 N.  FRW = 223.257 / 55.38795899 = 4.0
-                # if chromosome[-1] < 10 and chromosome[-2] < -0.9585 and chromosome[-3] < -17500: # best Y730  #187, 0.000902584 * 8050 * 9.8 = 71.204851760 N. FRW = 151.246 / 71.204851760 = 2.124
-                if chromosome[-1] < 11 and chromosome[-2] < -0.92 and chromosome[-3] < 250: # best severson02 #1130, 0.000830274 * 8050 * 9.8 = 65.50031586 N.  FRW = 177.418 / 65.5 = 2.7
-                    print(idx, swarm_project_names_Y730[idx], chromosome[::-1])
-
-                    def pyx_script():
-                        # Plot cross section view
-                        import population
-                        im_best = population.bearingless_induction_motor_design.local_design_variant(ad.spec.im_template, 99, 999, best_chromosome[:-3])
-                        im_best.ID = str(best_idx)
-                        pyx_draw_model(im_best)
-                        quit()
-
-                    # # Take high torque density design for LSA
-                    # if idx == 1625 - 1:
-                    #     best_idx = idx
-                    #     best_chromosome = chromosome
-                    #     pyx_script()
-
-                    # # Take high efficiency design for LSA
-                    # if idx == 187 - 1:
-                    #     best_idx = idx
-                    #     best_chromosome = chromosome
-                    #     pyx_script()
-
-                    # # Take low ripple performance design for LSA
-                    # if idx == 1130 - 1:
-                    #     best_idx = idx
-                    #     best_chromosome = chromosome
-                    #     # pyx_script()
-
-        # print('-'*40+'\nY730' + '\n      L_g,    w_st,   w_rt,   theta_so,   w_ro,    d_so,    d_ro,    -TRV,    -eta,    OC.')
-        print('-'*40+'\nY730' + '\n      _____________________________________________________________________________________')
-        selection_criteria(swarm_data_Y730)
-
-        # Set the output_dir back!
-        ad.solver.output_dir = ad.solver.fea_config_dict['dir_parent'] + ad.solver.fea_config_dict['run_folder']
-        quit()
-
-    print('Sizes of the 3 populations (in order):', len(swarm_data_severson01), len(swarm_data_severson02), len(swarm_data_Y730))
-    ad.solver.swarm_data = swarm_data_severson01 + swarm_data_severson02 + swarm_data_Y730 # list add
+    import one_script_pm_post_processing 
+    one_script_pm_post_processing.post_processing(ad, fea_config_dict)
     quit()
 
 #~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~

codes3/utility_moo.py
@@ -500,7 +500,7 @@ def pyx_draw_model(im):
         return np.cos(_)*x + np.sin(_)*y, -np.sin(_)*x + np.cos(_)*y
     def is_at_stator(im, path):
         return np.sqrt(path[0]**2 + path[1]**2) > im.Radius_OuterRotor + 0.5*im.Length_AirGap
-    
+
     for path in (vg.tikz.track_path): # track_path is passed by reference and is changed by mirror
         path_mirror = deepcopy(path)
         # for mirror copy (along x-axis)